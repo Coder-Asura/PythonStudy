40a8da8b5842bfc0ace824d8d63aeb849a7bb196
Asura <qq709238339@users.noreply.github.com>
2019-11-21 00:13:20 +0800

    Hello Python

---
+++ com/asura/python/Hello.py
@@ -0,0 +1,22 @@
+import keyword
+
+print("Hello Python")
+
+print("")
+
+print("下面是Python的保留字")
+print("保留字即关键字，我们不能把它们用作任何标识符名称n")
+# 打印关键字
+print(keyword.kwlist)
+
+# 这是注释
+
+'''
+这也是注释
+'''
+
+"""
+这还是注释
+"""
+
+print("上面这些是Python的保留字") # 你猜对了，这依然是注释
